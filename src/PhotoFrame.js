7cab5be038c5c46940ada4cbef24b46359a44520
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-17 16:56:59 +0200

    V2: Display 3 animals using input box

--- src/PhotoFrame.js
+++ src/PhotoFrame.js
@@ -1,18 +1,56 @@
 import React from 'react'
 
-export default function PhotoFrame({isVisible}) {
+export default function PhotoFrame({isVisible, animalNumber}) {
 
     if(isVisible)
     {
+        if(animalNumber == 1)
+        // display elephant cub
         return (
-            <div>
-                <img src="https://images5.alphacoders.com/342/342827.jpg" alt="Elephant calf" width="500"></img>
-            </div>
-          )
+            <>
+                <div>
+                    <img src="https://images5.alphacoders.com/342/342827.jpg" alt="Elephant calf" width="500"></img>
+                </div>
+                <label>An elephant cub!</label>
+            </>
+        );
+
+        if (animalNumber == 2)
+        // display gazelle
+        return (
+            <>
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Thompson_gazelle.jpg" alt="Gazelle" width="500"></img>
+                </div>
+                <label>A gazelle!</label>
+            </>
+        );
+
+        if (animalNumber == 3)
+        // display lion
+        return (
+            <>
+                <div>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/49/Male_Lion_on_Rock.jpg" alt="Lion" width="500"></img>
+                </div>
+                <label>A lion!</label>
+            </>
+        );
+
+        // Fallback case, display a placeholder photo...
+        // https://www.clipartqueen.com/image-files/animal-silhouette-tiger-big.jpg
+        return (
+            <>
+                <div>
+                    <img src="https://www.clipartqueen.com/image-files/animal-silhouette-tiger-big.jpg" alt="Placeholder photo - silhouette of an animal" width="500"></img>
+                </div>
+                <label>Invalid number!</label>
+            </>
+        );
     }
 
     return(
         <div></div>
-    )
+    );
   
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 /**
  * Author O.A.
  * Date october-16-2023
+ * 
+ * V2: Use a text box with number inputs, and make that decide which animal pic to display:
+ * 1 = elephant cub   https://images5.alphacoders.com/342/342827.jpg
+ * 2 = gazelle        https://upload.wikimedia.org/wikipedia/commons/9/9b/Thompson_gazelle.jpg
+ * 3 = lion           https://upload.wikimedia.org/wikipedia/commons/4/49/Male_Lion_on_Rock.jpg
  */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
