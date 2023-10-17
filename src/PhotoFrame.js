dbe0a2c4dc4973308a60b4262aeb680b2c2c9c27
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-17 17:09:14 +0200

    Refactor of v2

--- src/PhotoFrame.js
+++ src/PhotoFrame.js
@@ -2,49 +2,46 @@ import React from 'react'
 
 export default function PhotoFrame({isVisible, animalNumber}) {
 
+    let animalPicture = null;
+    let message = null;
+
+    if(animalNumber == 1){          // elephant cub
+        animalPicture =
+        <div>
+            <img src="https://images5.alphacoders.com/342/342827.jpg" alt="Elephant calf" width="500"></img>
+        </div>;
+
+        message = <label>An elephant cub</label>;
+    }
+    else if (animalNumber == 2){    // gazelle
+        animalPicture =
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Thompson_gazelle.jpg" alt="Gazelle" width="500"></img>
+        </div>;
+
+        message = <label>A gazelle</label>;
+    }
+    else if (animalNumber == 3){    // lion
+        animalPicture =
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/49/Male_Lion_on_Rock.jpg" alt="Lion" width="500"></img>
+        </div>;
+
+        message = <label>A lion</label>;
+    }
+    else{   // fallback case, placeholder photo
+        animalPicture =
+        <div>
+            <img src="https://www.clipartqueen.com/image-files/animal-silhouette-tiger-big.jpg" alt="Placeholder photo - silhouette of an animal" width="500"></img>
+        </div>
+    }
+
     if(isVisible)
     {
-        if(animalNumber == 1)
-        // display elephant cub
-        return (
+        return(
             <>
-                <div>
-                    <img src="https://images5.alphacoders.com/342/342827.jpg" alt="Elephant calf" width="500"></img>
-                </div>
-                <label>An elephant cub!</label>
-            </>
-        );
-
-        if (animalNumber == 2)
-        // display gazelle
-        return (
-            <>
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Thompson_gazelle.jpg" alt="Gazelle" width="500"></img>
-                </div>
-                <label>A gazelle!</label>
-            </>
-        );
-
-        if (animalNumber == 3)
-        // display lion
-        return (
-            <>
-                <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/49/Male_Lion_on_Rock.jpg" alt="Lion" width="500"></img>
-                </div>
-                <label>A lion!</label>
-            </>
-        );
-
-        // Fallback case, display a placeholder photo...
-        // https://www.clipartqueen.com/image-files/animal-silhouette-tiger-big.jpg
-        return (
-            <>
-                <div>
-                    <img src="https://www.clipartqueen.com/image-files/animal-silhouette-tiger-big.jpg" alt="Placeholder photo - silhouette of an animal" width="500"></img>
-                </div>
-                <label>Invalid number!</label>
+            {animalPicture}
+            {message}
             </>
         );
     }
