def4f3c5f67f43c76b3ede5935f6a9d530b38186
Oliver-10.10 <antaloliver@yahoo.com>
2023-10-31 18:21:22 +0100

    Preparing trains page for new dynamic elements: Added new static element

--- src/Pg2Trains.js
+++ src/Pg2Trains.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import TrainAnnouncementPanel from "./TrainAnnouncementPanel";
 import './Pg2Trains.css';
 
@@ -8,17 +8,20 @@ export default function Pg2Trains() {
     <>
       <h1>Trains page</h1>
 
+      {/** Picture of selected train type */}
       <div id="trainsPageContent">
-        
         <img src="https://www.railpictures.net/images/d1/1/5/3/6153.1375617413.jpg" alt="Romanian train" width = "500"></img>
-
         <br></br>
-
         <label>Picture owner: Titel Costica, <a href="https://www.railpictures.net/photo/445941/" target="_blank" rel="noopener noreferrer">RailPictures.Net</a></label>
       </div>
         
       <div id="trainsPageContent">
-        <TrainAnnouncementPanel/>  
+        <TrainAnnouncementPanel/>
+      </div>
+
+      {/** Picture of selected railway operator */}
+      <div id="trainsPageContent">
+        <img src="https://upload.wikimedia.org/wikipedia/en/e/ee/CFR_Calatori.png"></img>
       </div>
     </>
   )
