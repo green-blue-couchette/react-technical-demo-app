58d88c67e2d44f476c1282df59a82bdae04023be
Oliver-10.10 <antaloliver@yahoo.com>
2023-11-02 17:33:52 +0100

    Updated Author Information banner in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 /**
  * Author O.A.
- * Date october-20-2023
- * 
- * V3.1
- * 
- * TODO: 
- * x - Prepare to publish repository to GitHub...
- *    x - Rename this project to "react-technical-demo-app"
- *    x - Update title of the app and text descriptions where relevant.
- * 
+ * See the README at https://github.com/green-blue-couchette/react-technical-demo-app
  */
 
 import React from 'react';
