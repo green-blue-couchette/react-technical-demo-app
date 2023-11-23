613ae2ea179db39e2cca8ea50507eef6153625f0
Oliver-10.10 <antaloliver@yahoo.com>
2023-11-23 13:59:51 +0100

    Redid structure of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,33 @@ function App() {
 
   return (
     <>
-      <div style={{ textAlign: "center", color: "darkBlue", borderStyle: "dashed"}}>
-        <header>
+      <header>
         <p>
-          I built this web app to help myself re-learn React. <br></br>
-          Here, I collect different mini-applications to showcase my skills and try out ideas. I might incorporate different elements and ideas found here into other, standalone web apps. <br></br>
-          Things here may look incomplete or change. Stay tuned! - Oliver <br></br>
-          <br></br>
+          I built this web app to help myself re-learn React. 
+          Here, I collect different mini-applications to showcase my skills and try out ideas. I might incorporate different elements and ideas found here into other, standalone web apps. 
+          Things here may look incomplete or change. Stay tuned! - Oliver 
+        </p>
+        <p>  
           Navigate the pages using the navbar.
-          <br></br>
         </p>  
-        </header>
-      </div>
-      <br></br>
+      </header>
 
-      <div>
-
-        {/* START CODE FOR PAGE NAVIGATION*/}
-        <label>Pages: </label>
+      {/* START CODE FOR PAGE NAVIGATION*/}
+      <nav>
         <Link to="/">Home</Link>
         <Link to="/trains">Trains</Link>
         <hr></hr>
-        {/* END CODE FOR PAGE NAVIGATION*/}
+      </nav>
+      {/* END CODE FOR PAGE NAVIGATION*/}
         
+      <main>
         {/* START CODE FOR PAGE CONTENT */}
         <Routes>
           <Route path="/"         element={<h1>Use the navbar above!</h1>}/>
           <Route path="/trains"   element={<PgTrains/>}/>
         </Routes>
         {/* END CODE FOR PAGE CONTENT */}
-        
-      </div>
+      </main>
 
     </>
   );
